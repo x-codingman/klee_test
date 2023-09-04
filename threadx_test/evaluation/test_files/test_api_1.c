d661ff55b4f465dcbd20a344911b4708fc1fb8d2
x-codingman <644671785@qq.com>
2023-09-04 07:09:02 +0000

    update threadx testfiles

--- threadx_test/evaluation/test_files/test_api_1.c
+++ threadx_test/evaluation/test_files/test_api_1.c
@@ -2,16 +2,18 @@
 #include "stdint.h"
 
 #include "test_api.h"
+#include "klee/klee.h"
 
 void test_api_1(test_structure_1_t * test_ptr_1);
 
 int main(){
     test_structure_1_t *  test_ptr_1;
+    klee_make_symbolic_controllable(&test_ptr_1, sizeof(test_ptr_1), "test_ptr_1", 1);
     test_api_1(test_ptr_1);
 }
 
 void test_api_1(test_structure_1_t * test_ptr_1){
-    if((uint32_t)test_ptr_1 >= ATTACK_CAPABILITY_REGION_END && (uint32_t)test_ptr_1 <= ATTACK_CAPABILITY_REGION_START){
+    if(test_ptr_1 > ATTACK_CAPABILITY_REGION_END || test_ptr_1 < ATTACK_CAPABILITY_REGION_START){
         if(test_ptr_1->a == 0){
             *(test_ptr_1->p)=0;
         }
--- threadx_test/evaluation/test_files/test_api_2.c
+++ threadx_test/evaluation/test_files/test_api_2.c
@@ -2,15 +2,19 @@
 #include "stdint.h"
 
 #include "test_api.h"
-
+#include "klee/klee.h"
 void test_api_2(test_structure_2_t * test_ptr_2,int b, int c);
 int main(){
     test_structure_2_t *  test_ptr_2;
-    test_api_2(test_ptr_2);
+    int test_b,test_c;
+    klee_make_symbolic_controllable(&test_ptr_2, sizeof(test_ptr_2), "test_ptr_2", 1);
+    klee_make_symbolic_controllable(&test_b, sizeof(test_b), "test_b", 1);
+    klee_make_symbolic_controllable(&test_c, sizeof(test_c), "test_c", 1);
+    test_api_2(test_ptr_2,test_b,test_c);
 }
 
 void test_api_2(test_structure_2_t * test_ptr_2, int b, int c){
-    if((uint32_t)test_ptr_2 >= ATTACK_CAPABILITY_REGION_END && (uint32_t)test_ptr_2 <= ATTACK_CAPABILITY_REGION_START){
+    if(test_ptr_2 >= ATTACK_CAPABILITY_REGION_END || test_ptr_2 <= ATTACK_CAPABILITY_REGION_START){
         test_ptr_2->b = b;
         test_ptr_2->c = c;
     }
